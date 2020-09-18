65ba0c11f569cef6f4a0abf5e2eee3283fbce08c
Lucas Chaves <lucas.chaves@outlook.com>
2020-09-17 22:45:22 -0300

    fix swagger options

--- src/main/java/br/com/pi/apietl/config/SwaggerConfig.java
+++ src/main/java/br/com/pi/apietl/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.pi.apietl"))
-                .paths(regex("/api.*"))
+                .paths(regex("/.*"))
                 .build()
                 .apiInfo(metaInfo());
     }
